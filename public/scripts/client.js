b79e7733ecc796403ea333dd9fe45dcb427c9bda
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-07 14:31:22 -0600

    move html content to router files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,73 +30,84 @@ app.controller("HomeController", ["$scope", '$http', function($scope, $http) {
 
 app.controller("AddPetController", ["$scope", '$http', function($scope, $http) {
   console.log('add pet');
+
+  var clearForm = function() {
+    //reset input values to blank
+    $scope.nameIn = '';
+    $scope.ageIn = '';
+    $scope.speciesIn = '';
+    $scope.urlIn = '';
+  }; // end clearForm
+
+  var getPets = function() {
+    console.log('in getPets');
+    //get all pets from the server
+    $http({
+      method: 'GET',
+      url: '/pets'
+    }).then(function(response) {
+      console.log(response.data);
+      $scope.allPets = response.data;
+    }); // end $http
+  }; // end getPets
+
+  $scope.postPet = function() {
+    console.log('in postPet');
+    //assemble object to send
+    var objectToSend = {
+      name: $scope.nameIn,
+      animal: $scope.speciesIn,
+      age: Number($scope.ageIn),
+      image_url: $scope.urlIn
+    }; // end objectToSend
+    //send the object to the server
+    $http({
+      method: 'POST',
+      url: '/pets',
+      data: objectToSend
+    }).then(function(response) {
+      clearForm();
+      getPets();
+    }); // end $http
+  }; // end postPet
+
+
 }]); // end controller
 
 app.controller("ViewPetsController", ["$scope", '$http', function($scope, $http) {
   console.log('view pets');
-}]); // end controller
 
-// app.controller('PetsController', ['$scope', '$http', function($scope, $http) {
-//   console.log('NG');
-//
-//   var init = function() {
-//     getPets();
-//   }; // end init
-//
-//   var clearForm = function() {
-//     //reset input values to blank
-//     $scope.nameIn = '';
-//     $scope.ageIn = '';
-//     $scope.speciesIn = '';
-//     $scope.urlIn = '';
-//   }; // end clearForm
-//
-//   $scope.deletePet = function(petId) {
-//     console.log('in deletePet. ID -->', petId);
-//     //construct urlString
-//     var urlString = '/pets/' + petId;
-//     //send request to server to delete pet
-//     $http({
-//       method: 'DELETE',
-//       url: urlString,
-//     }).then(function(response) {
-//       console.log('delete $http success. Response -->',response);
-//       getPets();
-//     }); // end $http
-//   }; // end deletePet
-//
-//   var getPets = function() {
-//     console.log('in getPets');
-//     //get all pets from the server
-//     $http({
-//       method: 'GET',
-//       url: '/pets'
-//     }).then(function(response) {
-//       console.log(response.data);
-//       $scope.allPets = response.data;
-//     }); // end $http
-//   }; // end getPets
-//
-//   $scope.postPet = function() {
-//     console.log('in postPet');
-//     //assemble object to send
-//     var objectToSend = {
-//       name: $scope.nameIn,
-//       animal: $scope.speciesIn,
-//       age: Number($scope.ageIn),
-//       image_url: $scope.urlIn
-//     }; // end objectToSend
-//     //send the object to the server
-//     $http({
-//       method: 'POST',
-//       url: '/pets',
-//       data: objectToSend
-//     }).then(function(response) {
-//       clearForm();
-//       getPets();
-//     }); // end $http
-//   }; // end postPet
-//
-//   //initialize the app
-//   init();
-// }]); // end PetsController
+  var init = function() {
+    getPets();
+  }; // end init
+
+  $scope.deletePet = function(petId) {
+    console.log('in deletePet. ID -->', petId);
+    //construct urlString
+    var urlString = '/pets/' + petId;
+    //send request to server to delete pet
+    $http({
+      method: 'DELETE',
+      url: urlString,
+    }).then(function(response) {
+      console.log('delete $http success. Response -->',response);
+      getPets();
+    }); // end $http
+  }; // end deletePet
+
+  var getPets = function() {
+    console.log('in getPets');
+    //get all pets from the server
+    $http({
+      method: 'GET',
+      url: '/pets'
+    }).then(function(response) {
+      console.log(response.data);
+      $scope.allPets = response.data;
+    }); // end $http
+  }; // end getPets
+
+  //initialize the app
+  init();
+
+}]); // end controller
